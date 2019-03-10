f185b42ba56c385cc564ea3d6ae689f46d214d93
liyanfeng <957012567@qq.com>
2019-03-11 00:22:06 +0800

    添加单向环状链表

--- Llnode.py
+++ Llnode.py
@@ -133,6 +133,7 @@ class LList(object):
             p = p.next
 
 
+# 具有头节点指向尾节点的链表 (变形一)
 class LinkListHaveRear(LList):
     def __init__(self):
         super(LinkListHaveRear, self).__init__()
@@ -172,20 +173,94 @@ class LinkListHaveRear(LList):
         return e
 
 
+# 环状链表(变形二)
+class CircleLinkList(object):
+    def __init__(self):
+        self._rear = None
+
+    def is_empty(self):
+        return self._rear is None
+
+    def prepend(self, val):
+        p = LNode(val)
+        if self._rear is None:  # 空表
+            p.next = p  # 自己指向自己
+            self._rear = p
+        else:
+            # a->b  self._rear 现在是a next 是b 在 a和b中间加个p a->p->b
+            p.next = self._rear.next
+            self._rear.next = p
+
+    def append(self, val):
+        # 尾端插入 都是环了,所以和首段插入没差别
+        # 先在首部插入一个新节点
+        self.prepend(val)
+        # 此时首节点就变成了新节点,那么需要改变指针,将这个新节点的下一个节点设置成为首节点
+        self._rear = self._rear.next
+
+    # 弹出前一个
+    def pop(self):
+        # 空表
+        if self._rear is None:
+            raise LinkedListUnderflow('in pop of CircleLinkList')
+        # 获取首节点的下一个节点
+        p = self._rear.next
+        if self._rear is p:  # 判断这个表中是否只有一个节点
+            self._rear = None
+        else:
+            self._rear.next = p.next
+        return p.val
+
+    # 弹出后一个
+    def pop_last(self):
+        # 空表
+        if self._rear is None:
+            raise LinkedListUnderflow('in pop_last CircleLinkList')
+        # 获取当前节点的下一节点
+        p = self._rear.next
+        # 判断是否只有一个节点
+        if self._rear is p:
+            e = p.val
+            self._rear = None
+            return e
+        else:
+            while True:
+                # 如果p的下一个的下一个节点是首节点那么意味着p的下一个节点因该被pop掉
+                if p.next.next is self._rear:
+                    # 获取该被pop掉的那个节点
+                    e = p.next.val
+                    # p节点的下一个节点设为首节点,这样就把中间那个节点给隔掉了
+                    p.next = self._rear
+                    return e
+                p = p.next
+
+    def printall(self):
+        if self.is_empty():
+            return
+        p = self._rear.next
+        while True:
+            print(p.val, end='')
+            if p is self._rear:
+                print('')
+                break
+            p = p.next
+            print('->',end='')
+
+
 if __name__ == '__main__':
-    mlist_1 = LList()
-    for i in range(10):
-        # 首端插入
-        mlist_1.prepend(i)
-    for i in range(11, 20):
-        # 尾端插入
-        mlist_1.append(i)
+    # mlist_1 = LList()
+    # for i in range(10):
+    #     # 首端插入
+    #     mlist_1.prepend(i)
+    # for i in range(11, 20):
+    #     # 尾端插入
+    #     mlist_1.append(i)
     # 打印 方法测试
     # mlist_1.pop_last()
     # mlist_1.pop()
     # r = mlist_1.find(lambda x: x == 14)
     # print(r)
-    mlist_1.printall()
+    # mlist_1.printall()
 
     # 遍历操作测试
     # mlist_1.for_each(lambda x: print(f'val is {x}'))
@@ -199,8 +274,19 @@ if __name__ == '__main__':
     # find_all 测试
     # r = mlist_1.find_all(lambda x: x % 2 == 0)
     # print(list(r))
-    mlist_2 = LinkListHaveRear()
-    mlist_2.prepend(100)
-    for i in range(10):
-        mlist_2.append(i)
-    mlist_2.printall()
+    # mlist_2 = LinkListHaveRear()
+    # mlist_2.prepend(100)
+    # for i in range(10):
+    #     mlist_2.append(i)
+    # mlist_2.printall()
+
+    mlist_3 = CircleLinkList()
+    mlist_3.prepend(5),
+    mlist_3.prepend(4)
+    # mlist_3.printall()
+    for i in range(6, 10):
+        mlist_3.append(i)
+    mlist_3.printall()
+    # mlist_3.pop()
+    # mlist_3.pop_last()
+    mlist_3.printall()
